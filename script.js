56482e1862d1c6a53534d281c174e91d5bc2dfda
Minghui Liu <minghui.liu.95@gmail.com>
2021-04-22 08:06:47 +0800

    add second link

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-document.getElementById("apple").ondblclick=function(){myFunction()},document.getElementById("carrot").ondblclick=function(){myFunc2()};var state=!1;function myFunction(){state=!0}function myFunc2(){1==state&&(document.getElementById("ans").innerHTML=reverseString("5O5J0n3/yl.tib//:sptth"))}function reverseString(n){return n.split("").reverse().join("")}
+document.getElementById("apple").ondblclick = function() {
+    myFunction()
+},
+document.getElementById("carrot").ondblclick = function() {
+    myFunc1()
+},
+document.getElementById("pear").ondblclick = function() {
+    myFunc2()
+};
+var state = !1;
+function myFunction() {
+    state = !0
+}
+function myFunc2() {
+    1 == state && (document.getElementById("ans").innerHTML = reverseString("5O5J0n3/yl.tib//:sptth"))
+}
+function reverseString(n) {
+    return n.split("").reverse().join("")
+}
+function myFunc1() {
+    1 == state && (document.getElementById("ans").innerHTML = reverseString("9GMSO23/yl.tib//:sptth"))
+}
